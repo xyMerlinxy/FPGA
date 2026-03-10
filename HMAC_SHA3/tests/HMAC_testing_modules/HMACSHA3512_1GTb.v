56eb27642d1c6563e4b2a5601b17d4dee81dd153
xyMerlinxy <xymerlinxy@gmail.com>
2026-03-10 13:58:30 +0100

    HMAC_SHA3: add tests for tests in hardware for 1Gb data

---
+++ HMAC_SHA3/tests/HMAC_testing_modules/HMACSHA3512_1GTb.v
@@ -0,0 +1,50 @@
+module HMACSHA3512_1GTb;
+
+reg [7:0] TEST = 8'b0;
+integer i;
+/////////////////////////////////////////
+
+parameter inClkp = 10;
+reg       inClk  = 1'b0;
+
+always
+begin
+    #(inClkp/2) inClk = !inClk;
+end
+
+////////////////////////////////////////
+reg   regReset = 1'b0;
+
+wire         outBusy;
+wire         outCorrect;
+
+HMACSHA3512_1G HMACSHA3512_1G_inst(
+   .inClk(inClk),
+   .inRst(regReset),
+   .outCorrect(outCorrect),
+   .outBusy(outBusy)
+);
+
+reg regWork = 1'b1;
+
+always
+begin
+   #(inClkp);
+
+    while(regWork == 1'b1 ) begin
+		#(10000*inClkp);
+		if(outBusy == 1'b0 ) begin
+			regWork <= 1'b0;
+		end
+		$display("WORKING %d %d", HMACSHA3512_1G_inst.regCounter, regWork);
+	end
+    if(outCorrect == 1'b1) begin
+       $display("Test passed.");
+    end else begin
+        $display("Test failed!");
+    end
+   $stop;
+end
+
+endmodule
+
