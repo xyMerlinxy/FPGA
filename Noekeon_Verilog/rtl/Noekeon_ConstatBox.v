f79771b654f39d6d62a481f21570137a23a78d14
xyMerlinxy <xymerlinxy@gmail.com>
2026-03-10 13:58:59 +0100

    fix(Noekeon): rename Number to Counter

--- Noekeon_Verilog/rtl/Noekeon_ConstatBox.v
+++ Noekeon_Verilog/rtl/Noekeon_ConstatBox.v
@@ -2,28 +2,28 @@
 // This module is implemented as combinatorial logic
 
 module NoekeonConstantBox(
-  input wire  [4:0]    inRoundNum,
+  input wire  [4:0]    inRoundCnt,
   output wire  [7:0]    outRoundConst
 
 );
 
 assign outRoundConst =
-  (inRoundNum == 5'd00) ? 8'h80:
-  (inRoundNum == 5'd01) ? 8'h1B:
-  (inRoundNum == 5'd02) ? 8'h36:
-  (inRoundNum == 5'd03) ? 8'h6C:
-  (inRoundNum == 5'd04) ? 8'hD8:
-  (inRoundNum == 5'd05) ? 8'hAB:
-  (inRoundNum == 5'd06) ? 8'h4D:
-  (inRoundNum == 5'd07) ? 8'h9A:
-  (inRoundNum == 5'd08) ? 8'h2F:
-  (inRoundNum == 5'd09) ? 8'h5E:
-  (inRoundNum == 5'd10) ? 8'hBC:
-  (inRoundNum == 5'd11) ? 8'h63:
-  (inRoundNum == 5'd12) ? 8'hC6:
-  (inRoundNum == 5'd13) ? 8'h97:
-  (inRoundNum == 5'd14) ? 8'h35:
-  (inRoundNum == 5'd15) ? 8'h6A:
+  (inRoundCnt == 5'd00) ? 8'h80:
+  (inRoundCnt == 5'd01) ? 8'h1B:
+  (inRoundCnt == 5'd02) ? 8'h36:
+  (inRoundCnt == 5'd03) ? 8'h6C:
+  (inRoundCnt == 5'd04) ? 8'hD8:
+  (inRoundCnt == 5'd05) ? 8'hAB:
+  (inRoundCnt == 5'd06) ? 8'h4D:
+  (inRoundCnt == 5'd07) ? 8'h9A:
+  (inRoundCnt == 5'd08) ? 8'h2F:
+  (inRoundCnt == 5'd09) ? 8'h5E:
+  (inRoundCnt == 5'd10) ? 8'hBC:
+  (inRoundCnt == 5'd11) ? 8'h63:
+  (inRoundCnt == 5'd12) ? 8'hC6:
+  (inRoundCnt == 5'd13) ? 8'h97:
+  (inRoundCnt == 5'd14) ? 8'h35:
+  (inRoundCnt == 5'd15) ? 8'h6A:
   8'hD4;
 
 endmodule
