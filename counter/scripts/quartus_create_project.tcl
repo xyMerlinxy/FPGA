e84801d1e9836f38be02439a54df57d55fcb3069
xyMerlinxy <xymerlinxy@gmail.com>
2026-03-20 19:01:51 +0100

    add(counter): add script to generate quartus project

---
+++ counter/scripts/quartus_create_project.tcl
@@ -0,0 +1,28 @@
+# Create project
+project_new lfsr_counter -overwrite
+
+# Device settings - Cyclone V on DE0-CV
+set_global_assignment -name FAMILY "Cyclone V"
+set_global_assignment -name DEVICE 5CEBA4F23C7
+
+# Project settings
+set_global_assignment -name TOP_LEVEL_ENTITY lfsr_counter
+set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
+
+# VHDL version
+set_global_assignment -name VHDL_INPUT_VERSION VHDL_1993
+
+# Add all VHDL files from rtl and subdirectories
+foreach f [glob -nocomplain ../../rtl/*.vhd] {
+    set_global_assignment -name VHDL_FILE $f
+}
+foreach f [glob -nocomplain ../../rtl/**/*.vhd] {
+    set_global_assignment -name VHDL_FILE $f
+}
+
+foreach f [glob -nocomplain ../../sdc/*.sdc] {
+    set_global_assignment -name SDC_FILE $f
+}
+
+# Save and close
+export_assignments
